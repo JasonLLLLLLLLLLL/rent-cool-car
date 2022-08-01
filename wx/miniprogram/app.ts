85d6a220d861b6f837965da3ae3cb11435e64f8a
jeffrey <1319429605@qq.com>
2022-08-01 21:50:51 +0800

    to be continued

--- wx/miniprogram/app.ts
+++ wx/miniprogram/app.ts
@@ -1,8 +1,13 @@
+import camelcaseKeys from "camelcase-keys"
 import { IAppOption } from "./appoption"
+import { coolcar } from "./service/proto_gen/trip_pb"
 
+let resolveUserInfo: (value: WechatMiniprogram.UserInfo | PromiseLike<WechatMiniprogram.UserInfo>) => void
+let rejectUserInfo: (reason?: any) => void
 // app.ts
 App<IAppOption>({
-  globalData: {},
+  globalData: {
+  },
   async onLaunch() {
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
@@ -12,16 +17,20 @@ App<IAppOption>({
       url: 'http://localhost:8080/trip/trip123',
       method: 'GET',
       success: res => {
-        const getTripResp = res.data
+        const getTripResp = coolcar.GetTripResponse.fromObject(camelcaseKeys( res.data as object))
+        console.log(getTripResp)
+        console.log('status is', coolcar.TripStatus[getTripResp.trip?.status!])
       },
-      fail: console.error,
+      fail: console.error, 
     })
     // 登录
     wx.login({
       success: res => {
         console.log(res.code)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        // openid对应一个用户
+        // 
       },
     })
-  },
+  }, 
 })
